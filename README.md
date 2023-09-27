4100ed09cf8fd9cc5d0ff6790e39b9dee2abe19b
Noob-Ovi <abdullah.ovi098@gmail.com>
2023-09-27 07:53:16 +0600

    donation route feature added

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ### Features: 
-    - Users can see details before donating any sector by just clicking on the donation card. 
-    - Users can filter category-wise donation by searching. 
-    - Users can see the donated sector in thee **Donation** page. 
-    - Users can see the statistics of donated and total percentage in the **Statistics** page.
+   * Users can see details before donating any sector by just clicking on the donation card. 
+   * Users can filter category-wise donation by searching. 
+   * Users can see the donated sector in thee **Donation** page. 
+   * Users can see the statistics of donated and total percentage in the **Statistics** page.
--- src/Pages/Donation/Donation.jsx
+++ src/Pages/Donation/Donation.jsx
@@ -6,6 +6,7 @@ const Donation = () => {
 
     const [donationList,setDonationList] = useState([])
     const [seeMore,setSeeMore] = useState(false)
+    const [noDonation,setNoDonation] = useState(false)
     
    
     const handleSee=()=>{
@@ -15,12 +16,24 @@ const Donation = () => {
     
     useEffect(()=>{
         const fromLocalStorage = JSON.parse(localStorage.getItem('addedDonationList'))
-        setDonationList(fromLocalStorage)
-        
+        if(fromLocalStorage){
+            setDonationList(fromLocalStorage)
+        }
+        else{
+            setNoDonation(true)
+        }
     },[])
     return (
        <div>
-         <div className="grid grid-cols-1 lg:grid-cols-2 w-[90%]  md:w-[60%] lg:w-[80%] mx-auto my-7 gap-5">
+         {
+            noDonation ? 
+            <div className="h-[67vh] flex flex-col justify-center items-center">
+                <h1 className="text-3xl text-red-500 font-bold text-center">Not donated yet?</h1>
+                <h1 className="text-3xl text-violet-500 font-bold text-center">Your small contribution can bring joy 😊 </h1>
+            </div>
+            :
+            <div>
+                <div className="grid grid-cols-1 lg:grid-cols-2 w-[90%]  md:w-[60%] lg:w-[80%] mx-auto my-7 gap-5">
            {
               seeMore ?  donationList?.map((donation,idx)=><DonationCard key={idx} donation={donation}></DonationCard>)
               :
@@ -32,6 +45,8 @@ const Donation = () => {
             <button onClick={handleSee} className={`p-[5px] rounded bg-orange-400 text-white ${donationList?.length > 4 || "hidden"} font-medium `}>{seeMore ? "See less" : "See All"} </button>
         }
         </div>
+            </div>
+         }
        </div>
     );
 };
